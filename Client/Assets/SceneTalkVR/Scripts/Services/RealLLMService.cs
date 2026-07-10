1eeda0510d7f29c651da95663f7a72fd8974b780
spring5 <spring5@sjtu.edu.cn>
2026-07-10 15:55:06 +0800

    fix(llm): resolve GatewaySpeechInputModule missing namespace compiler error

--- Client/Assets/SceneTalkVR/Scripts/Services/RealLLMService.cs
+++ Client/Assets/SceneTalkVR/Scripts/Services/RealLLMService.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 using SceneTalkVR.Core;
+using SceneTalkVR.Voice;
 using UnityEngine;
 using UnityEngine.Networking;
 
